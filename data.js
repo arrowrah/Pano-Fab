1b70b2bceb057eb94b1d4adc905ffdb772bbd5a8
arrowrah <arrowrah@gmail.com>
2024-01-19 14:36:55 -0800

    Pano-Fab

---
+++ data.js
@@ -0,0 +1,162 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-veev_fab_5",
+      "name": "veev_fab_5",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2587,
+      "initialViewParameters": {
+        "yaw": -2.280388042177492,
+        "pitch": 0.16025240484733771,
+        "fov": 0.9410955196928921
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.5049514703567315,
+          "pitch": 0.5343197079663913,
+          "rotation": 6.283185307179586,
+          "target": "1-veev_fab_1"
+        },
+        {
+          "yaw": -2.7324725786726756,
+          "pitch": 0.49405674472683714,
+          "rotation": 6.283185307179586,
+          "target": "2-veev_fab_2"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.14002295122992,
+          "pitch": 0.37548855636710776,
+          "title": "Veev Build",
+          "text": "Union City, CA, USA"
+        }
+      ]
+    },
+    {
+      "id": "1-veev_fab_1",
+      "name": "veev_fab_1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2586,
+      "initialViewParameters": {
+        "yaw": -2.1036004779070225,
+        "pitch": 0.08033139781840681,
+        "fov": 1.3108730938557924
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.4351753189966594,
+          "pitch": 0.4582899367432418,
+          "rotation": 6.283185307179586,
+          "target": "0-veev_fab_5"
+        },
+        {
+          "yaw": -2.227117193770452,
+          "pitch": 0.4999021839113915,
+          "rotation": 0,
+          "target": "2-veev_fab_2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-veev_fab_2",
+      "name": "veev_fab_2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2585.5,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.383486112025201,
+          "pitch": 0.5734303756963648,
+          "rotation": 0,
+          "target": "0-veev_fab_5"
+        },
+        {
+          "yaw": -2.1597825727888846,
+          "pitch": 0.5995065394447412,
+          "rotation": 0,
+          "target": "1-veev_fab_1"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Veev FAB",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
